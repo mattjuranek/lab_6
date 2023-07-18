3eb6ca575672410e3d5f5a13c00813b87e2fc0e7
mattjuranek <139582278+mattjuranek@users.noreply.github.com>
2023-07-18 16:44:21 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# Matthew Juranek
+def menu():
+    print("Menu\n"
+          "-------------\n"
+          "1. Encode\n"
+          "2. Decode\n"
+          "3. Quit\n")
+
+
+def encode(original_password):
+    encoded_password = ""
+    for digit in original_password:
+        new_digit = int(digit) + 3
+        new_digit = str(new_digit)
+        if len(new_digit) > 1:
+            new_digit = new_digit[1]
+        encoded_password += str(new_digit)
+    return encoded_password
+
+
+
+def main():
+    while True:
+        menu()
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            original_password = str(input("Please enter your password to encode: "))
+            encoded_password = encode(original_password)
+            print("Your password has been encoded and stored!\n")
+        elif option == 2:
+            pass
+        elif option == 3:
+            break
+
+
+if __name__ == "__main__":
+    main()
+
